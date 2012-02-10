c180593355fc0f03a0d3ef9d29e8a491d6c4ce89
flo <burna1@gmail.com>
2012-02-10 19:09:06 +0100

    Fix Issue#39, UserPanel teilweise weiss

--- sim/src/main/java/devhood/im/sim/ui/UserPanel.java
+++ sim/src/main/java/devhood/im/sim/ui/UserPanel.java
@@ -65,24 +65,29 @@ public class UserPanel extends JPanel {
 		BoxLayout layout = new BoxLayout(this, BoxLayout.PAGE_AXIS);
 		setLayout(layout);
 
-		addUsers();
+		for (JLabel userLabel : getCurrentUsers()) {
+			add(userLabel);
+		}
+
 		setUserChangeListener();
 		startRefreshUsers();
 	}
 
 	/**
-	 * Fuegt die Users aus registryService in die Liste ein.
+	 * Gibt die Labels der aktuellen User aus der DB zurueck.
 	 */
-	public void addUsers() {
+	public List<JLabel> getCurrentUsers() {
 		List<User> users = userService.getUsers();
 		List<String> userFromDb = new ArrayList<String>();
+		List<JLabel> currentUsers = new ArrayList<JLabel>();
 
 		for (User user : users) {
 			userFromDb.add(user.getName());
 			final JLabel userLabel = createUserLabel(user);
-			add(userLabel);
-
+			currentUsers.add(userLabel);
 		}
+
+		return currentUsers;
 	}
 
 	/**
@@ -220,9 +225,16 @@ public class UserPanel extends JPanel {
 	 * Aktualisiert das UserPanel.
 	 */
 	public void refreshUi() {
+		List<JLabel> userLabels = getCurrentUsers();
+
 		removeAll();
-		addUsers();
+
+		for (JLabel userLabel : userLabels) {
+			add(userLabel);
+		}
+
 		validate();
 		repaint();
 	}
+
 }

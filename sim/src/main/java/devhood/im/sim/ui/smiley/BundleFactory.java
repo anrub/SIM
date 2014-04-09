7f3196da97e236ad0f3eb3648bf9d09b486578f6
flo <fuchsfl@googlemail.com>
2014-04-09 18:08:39 +0200

    Fix, fuer Windows

--- sim/src/main/java/devhood/im/sim/ui/smiley/BundleFactory.java
+++ sim/src/main/java/devhood/im/sim/ui/smiley/BundleFactory.java
@@ -2,12 +2,16 @@ package devhood.im.sim.ui.smiley;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 abstract public class BundleFactory {
@@ -25,26 +29,32 @@ abstract public class BundleFactory {
 	 */
 	public void scanTree(URI startDirectory, final String filePattern)
 			throws IOException {
-		Files.walkFileTree(Paths.get(startDirectory),
-				new SimpleFileVisitor<Path>() {
-					private Pattern pattern = Pattern.compile(filePattern);
+		Path path = Paths.get(startDirectory);
+		if (startDirectory.toString().contains("jar:")) {
+			final Map<String, String> env = new HashMap<>();
+			final String[] array = startDirectory.toString().split("!");
+			final FileSystem fs = FileSystems.newFileSystem(
+					URI.create(array[0]), env);
+			path = fs.getPath(array[1]);
+		}
+		Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+			private Pattern pattern = Pattern.compile(filePattern);
 
-					@Override
-					public FileVisitResult visitFile(Path file,
-							BasicFileAttributes attrs) throws IOException {
-						boolean matches = pattern.matcher(file.toString())
-								.matches();
-						if (matches) {
-							try {
-								evaluateFile(file);
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-						return FileVisitResult.CONTINUE;
+			@Override
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				boolean matches = pattern.matcher(file.toString()).matches();
+				if (matches) {
+					try {
+						evaluateFile(file);
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
+				}
+				return FileVisitResult.CONTINUE;
+			}
 
-				});
+		});
 	}
 
 	abstract public void evaluateFile(Path file) throws Exception;

a1a89d05c9d2b64463b047c6760aa8fd76e437d2
Tobias Zeising <Zeising.Tobias@swm.de>
2014-05-12 08:32:43 +0200

    new webui

---
+++ docs/webui/js/sim-backend-dummy.js
@@ -0,0 +1,223 @@
+var messages = [
+                {
+                    "datetime": "1399632845",
+                    "sender": "Zeising.Tobias",
+                    "email": "tobias.zeising@aditu.de",
+                    "receiver" : "receiver",
+                    "text": "ubergren, no sea takimata ;) sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                },
+                {
+                    "datetime": "1399632845",
+                    "sender": "Zeising.Tobias",
+                    "email": "tobias.zeising@aditu.de",
+                    "receiver" : "er9wer",
+                    "text": "ipsum dolor sit amet. :pirat: :clown: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                }
+            ];
+
+
+sim.backend = {
+
+    // register callbacks
+
+    /**
+     * register callback for a new online user
+     */
+    onUserOnlineNotice: function(callback) {
+        sim.backend.userOnlineNoticeCallback = callback;
+    },
+    
+    /**
+     * register callback for a user goes offline
+     */
+    onUserOfflineNotice: function(callback) {
+        sim.backend.userOfflineNoticeCallback = callback;
+    },
+    
+    /**
+     * register callback for user changes his state
+     */
+    onUserUpdateNotice: function(callback) {
+        sim.backend.userUpdateNotice = callback;
+    },
+    
+    /**
+     * register callback for incoming new message
+     */
+    onNewMessage: function(callback) {
+        sim.backend.newMessage = callback;
+    },
+    
+    /**
+     * register callback for incoming file
+     */
+    onFileSendRequest: function(callback) {
+        sim.backend.fileSendRequest = callback;
+    },
+    
+    /**
+     * register callback for new file progress state
+     */
+    onFileSendProgress: function(callback) {
+        sim.backend.fileSendProgress = callback;
+    },
+    
+    /**
+     * register callback for user enters room
+     */
+    onRoomEnterNotice: function(callback) {
+        sim.backend.roomEnterNotice = callback;
+    },
+    
+    /**
+     * register callback for user leaves room
+     */
+    onRoomLeaveNotice: function(callback) {
+        sim.backend.roomLeaveNotice = callback;
+    },
+    
+    /**
+     * register callback for new room was opened
+     */
+    onRoomOpened: function(callback) {
+        sim.backend.roomOpened = callback;
+    },
+    
+    /**
+     * register callback for room list update
+     */
+    onGetRoomlistResponse: function(callback) {
+        sim.backend.getRoomlistResponse = callback;
+    },
+    
+    /**
+     * register callback for user list update
+     */
+    onGetUserlistResponse: function(callback) {
+        sim.backend.getUserlistResponse = callback;
+    },
+    
+    /**
+     * register callback for getting converstion
+     */
+    onGetContentResponse: function(callback) {
+        sim.backend.getContentResponse = callback;
+    },
+    
+    /**
+     * register callback for error message
+     */
+    onError: function(callback) {
+        sim.backend.error = callback;
+    },
+    
+    
+    
+    // functions for frontend
+    
+    
+    /**
+     * prompt backend for new userlist. onGetUserlistRespons will be executed.
+     */
+    updateUserlist: function() {
+        if(typeof sim.backend.getUserlistResponse != "undefined") {
+            sim.backend.getUserlistResponse([
+                {username: "Zeising.Tobias", rooms: ['Webteam', 'swm', 'Blub'], state: 'AVAILABLE', email: 'tobias.zeising@aditu.de'},
+                {username: "Fuchs.Florian", rooms: ['Webteam'], state: 'NOT_AVAILABLE', email: 'fuchsi@bla.de'},
+                {username: "Koslovski.Erich", rooms: [], state: 'AVAILABLE', email: 'erich@bla.de'}
+            ]);
+        }
+    },
+    
+    
+    /**
+     * prompt backend for new roomlist. onGetRoomlistResponse will be executed.
+     */
+    updateRoomlist: function() {
+        if(typeof sim.backend.getRoomlistResponse != "undefined") {
+            sim.backend.getRoomlistResponse(['Webteam', 'swm', 'Blub']);
+        }
+    },
+    
+    
+    /**
+     * prompt backend for conversations. id is username or roomname. onGetContentResponse will be executed.
+     */
+    getConversation: function(id) {
+        if(typeof sim.backend.getContentResponse != "undefined") {
+            sim.backend.getContentResponse(id, messages);
+        }
+    },
+
+    
+    /**
+     * receiving file will be rejected
+     */
+    fileSendReject: function(uuid) {
+    
+    },
+    
+    
+    /**
+     * receiving file will be accepted
+     */
+    fileSendAccept: function(uuid, port) {
+    
+    },
+    
+    
+    /**
+     * send new message. receiver is username or roomname.
+     */
+    sendMessage: function(receiver, text) {
+        var now = new Date().getTime() / 1000;
+        messages[messages.length] = {
+            "datetime": now,
+            "sender": "Zeising.Tobias",
+            "email": "tobias.zeising@aditu.de",
+            "receiver" : receiver,
+            "text": text
+        };
+        sim.backend.getContentResponse(receiver, messages);
+    },
+    
+    
+    /**
+     * update own status.
+     * status = AVAILABLE oder BUSY oder NOT_AVAILABLE
+     */
+    updateStatus: function(status) {
+    
+    },
+    
+    
+    /**
+     * open new room
+     */
+    openRoom: function(name) {
+    
+    },
+    
+    
+    /**
+     * join room
+     */
+    joinRoom: function(name) {
+    
+    },
+    
+    
+    /**
+     * leave room
+     */
+    leaveRoom: function(name) {
+    
+    }
+
+};
+
+
+
+
+
+
--- docs/webui/js/sim-base.js
+++ docs/webui/js/sim-base.js
@@ -1,15 +1,20 @@
 var sim = {
 
     /**
-     * all received and sent chat messages
+     * counts unread messages for every room/person
      */
-    messages: false,
-
+    unreadMessagesCounter: {},
+    
+    /**
+     * id of current visible conversation
+     */
+    currentConversation: false,
+    
     /**
      * initialize application
      */
     init: function() {
-        jQuery(document).ready(function() {
+        jQuery(document).ready(function() {          
             // load emoticons
             sim.initEmoticons();
             
@@ -26,20 +31,111 @@ var sim = {
                     updateOnContentResize: true,
                 }
             });
+            
+            // initialize backend callbacks
+            sim.initBackendCallbacks();
+            
+            // Userliste und Rooms updaten
+            sim.backend.updateUserlist();
+            sim.backend.updateRoomlist();
         });
     },
     
     
     /**
-     * Emoticons initialisieren
+     * set callbacks which will update the frontend on backend action (e.g. receiving a new message)
+     */
+    initBackendCallbacks: function() {
+        // alertify about new user state
+        var userOnlineOffline = function(text) {
+            alertify.log(text);
+            sim.backend.updateUserlist();
+            
+            // update messagelist for updating online/offline state
+            sim.backend.getConversation(sim.currentConversation);
+        }
+        
+        // user is now online
+        sim.backend.onUserOnlineNotice(userOnlineOffline);
+        
+        // register callback for a user goes offline
+        sim.backend.onUserOfflineNotice(userOnlineOffline);
+        
+        // register callback for user changes his state
+        sim.backend.onUserUpdateNotice(function(id, text) {
+            alertify.log("Status: " + id + " " + text);
+            sim.backend.updateUserlist();
+        });
+        
+        // register callback for incoming new message
+        sim.backend.onNewMessage(function(message) {
+            if(sim.currentConversation == message.receiver)
+                sim.backend.getConversation(sim.currentConversation);
+            else {
+                if (typeof sim.unreadMessagesCounter[message.receiver] == "undefined") {
+                    sim.unreadMessagesCounter[message.receiver] = 0;
+                }
+                sim.unreadMessagesCounter[message.receiver]++;
+                sim.backend.updateUserlist();
+                sim.backend.updateRoomlist();
+            }
+        });
+        
+        // register callback for incoming file
+        sim.backend.onFileSendRequest(function() {
+            // ToDo
+        });
+        
+        // register callback for new file progress state
+        sim.backend.onFileSendProgress(function() {
+            // ToDo
+        });
+        
+        // register callback for user enters room
+        sim.backend.onRoomEnterNotice(function(room, user) {
+            alertify.log("Benutzer: " + user + " hat den Raum " + room + " betreten");
+            sim.backend.updateUserlist();
+        });
+        
+        // register callback for user leaves room
+        sim.backend.onRoomLeaveNotice(function() {
+            alertify.log("Benutzer: " + user + " hat den Raum " + room + " verlassen");
+            sim.backend.updateUserlist();
+        });
+        
+        // register callback for new room was opened
+        sim.backend.onRoomOpened(function(room) {
+            alertify.log("Neuer Raum " + room + " wurde eröffnet");
+            sim.backend.updateRoomlist();
+        });
+        
+        // register callback for room list update
+        sim.backend.onGetRoomlistResponse(function(rooms) {
+            sim.updateRoomlist(rooms);
+        });
+        
+        // register callback for user list update
+        sim.backend.onGetUserlistResponse(function(users) {
+            sim.updateUserlist(users);
+        });
+        
+        // register callback for getting conversation
+        sim.backend.onGetContentResponse(function(id, messages) {
+            if (id==sim.currentConversation)
+                sim.updateConversation(messages);
+        });
+    },
+    
+    
+    /**
+     * load all available emoticons
      */
     initEmoticons: function() {
         var emotbox = $('#message-emoticons');
         var lastEmot = "";
         $.each(emoticons, function(shortcut, emoticon) {
-            if(lastEmot != emoticon) {
+            if(lastEmot != emoticon)
                 emotbox.append('<img src="'+ emoticon +'" title="' + shortcut + '"/>');
-            }
             lastEmot = emoticon;
         });
     },
@@ -70,9 +166,8 @@ var sim = {
             var user = $(this).find('.contacts-name').html();
             $('.rooms li, .contacts li').removeClass('active');
             $(this).addClass('active');
-            
-            // ToDo
-            console.info("open chat user " + user); // highlite new messages, set unreadcounter to 0
+            sim.currentConversation = user;
+            sim.backend.getConversation(user);
         });
         
         // enter room click
@@ -80,32 +175,164 @@ var sim = {
             var room = $(this).parent().find('.name').html();
             $('.rooms li, .contacts li').removeClass('active');
             $(this).parent().parent().addClass('active');
-            
-            // ToDo
-            console.info("enter and open room " + room);
+            sim.currentConversation = room;
+            sim.backend.joinRoom(room);
+            sim.backend.updateRoomlist();
+            sim.backend.getConversation(sim.currentConversation);
         });
         
         // leave room click
         $('.rooms').delegate("li .rooms-leave", "click", function(e) {
             var room = $(this).parent().find('.name').html();
-            
-            // ToDo
-            console.info("leave room " + room);
+            sim.backend.leaveRoom(room);
         });
         
         // send message
         $('#message-send').click(function() {
             var message = $('#message-input-textfield').val();
-            
-            // ToDo
-            console.info("send message " + message);
-            
-            // scroll 2 bottom
-            $("#content-wrapper").mCustomScrollbar("scrollTo","bottom");
+            $('#message-input-textfield').val("");
+            sim.backend.sendMessage(sim.currentConversation, message);
+            sim.backend.updateRoomlist();
         });
     },
     
     
+    
+    // ui update helpers
+    
+    
+    /**
+     * update current userlist
+     */
+    updateUserlist: function(users) {
+        // save scroll state
+        var scrollPosition = $("#contacts-wrapper").scrollTop();
+        
+        // update userlist
+        $('.contacts').html('');
+        $.each(users, function(index, user) {
+            // unread
+            var unread = "";
+            if (typeof sim.unreadMessagesCounter[user.username] != "undefined")
+                unread = '<div class="contacts-unread">' + sim.unreadMessagesCounter[user.username] + '</div>';
+            
+            // online state
+            var state = 'online';
+            if(user.state.toLowerCase()=='not_available')
+                state = 'offline';
+            if(user.state.toLowerCase()=='busy')
+                state = 'notavailable';
+            
+            // avatar url
+            var avatar = sim.gravatarUrl(user.email);
+            
+            // active
+            var active = '';
+            if(sim.currentConversation==user.username)
+                active = 'class="active"';
+            
+            $('.contacts').append('<li ' + active + '>\
+                <div class="' + state + ' contacts-state"></div>\
+                <img src="' + avatar + '" class="contacts-avatar avatar" />\
+                <div class="contacts-name">' + sim.escape(user.username) + '</div>\
+                ' + unread + '\
+            </li>');
+        });
+        
+        // restore scroll state
+        $("#contacts-wrapper").mCustomScrollbar("scrollTo", scrollPosition);
+    },
+    
+    
+    /**
+     * update roomlist
+     */
+    updateRoomlist: function(rooms) {
+        // save scroll state
+        var scrollPosition = $("#rooms-wrapper").scrollTop();
+        
+        // update roomlist
+        $('.rooms').html('');
+        $.each(rooms, function(index, room) {
+            // state
+            var state = 'rooms-outside';
+            var leave = '';
+            if($.inArray(room, rooms.usersrooms)) {
+                state = 'rooms-inside';
+                leave = '<span class="rooms-leave ion-log-out"></span>';
+            }
+            
+            // unread
+            var unread = "";
+            if (typeof sim.unreadMessagesCounter[room] != "undefined")
+                unread = '<div class="contacts-unread">' + sim.unreadMessagesCounter[room] + '</div>';
+
+            // active
+            var active = '';
+            if(sim.currentConversation==room)
+                active = 'class="active"';
+                
+            $('.rooms').append('<li ' + active + '>\
+                <div class="' + state + '"></div> \
+                <div class="rooms-name"><span class="name">' + sim.escape(room) + '</span> ' + leave + ' </div>\
+                ' + unread + '\
+            </li>');
+        });
+        
+        // restore scroll state
+        $("#rooms-wrapper").mCustomScrollbar("scrollTo", scrollPosition);
+    },
+    
+    
+    /**
+     * update current conversation
+     */
+    updateConversation: function(messages) {
+        // set unreadcounter to 0
+        var unread = sim.unreadMessagesCounter[sim.currentConversation];
+        delete sim.unreadMessagesCounter[sim.currentConversation];
+        sim.backend.updateUserlist();
+        sim.backend.updateRoomlist();
+        
+        // set metadata: avatar
+        var avatar = '';
+        if($('.contacts .active').length > 0)
+            avatar = '<img src="' + $('.contacts .active .avatar').attr('src') + '" class="avatar" />';
+        
+        // set metadata: state
+        var state = 'online';
+        var stateElement = $('.active > div:first');
+        if(stateElement.length > 0) {
+            if(stateElement.hasClass('offline')) {
+                state = 'offline';
+            } else if(stateElement.hasClass('notavailable')) {
+                state = 'notavailable';
+            }
+        }
+        
+        // write metadata
+        $('#main-metadata').html(avatar + '<span>' + sim.currentConversation + '</span><span class="' + state + '"></span>');
+        
+        // show messages (highlite new messages)
+        $('#content').html('');
+        $.each(messages, function(index, message) {
+            $('#content').append('<li class="entry">\
+                <div class="entry-metadata">\
+                    <img src="' + sim.gravatarUrl(message.email) + '" class="avatar" />\
+                    <span>' + sim.escape(message.sender) + '</span>\
+                    <span class="entry-datetime">' + sim.dateAgo(message.datetime) + '</span>\
+                </div>\
+                <div class="entry-content">\
+                    ' + sim.emoticons(sim.escape(message.text)) + '\
+                </div>\
+            </li>');
+        });
+        
+        // scroll 2 bottom
+        $("#content-wrapper").mCustomScrollbar("scrollTo","bottom");
+    },
+    
+        
     /**
      * set automatically the height of the tags and set scrollbar for div scrolling
      */
@@ -122,5 +349,69 @@ var sim = {
         var padding = parseInt($('#content').css('padding-bottom')) * 2;
         
         $('#content-wrapper').height(windowHeight - headerHeight - messageHeight - padding);
+    },
+        
+    
+    
+    // private helpers
+    
+    /**
+     * return gravatar image if available
+     */
+    gravatarUrl: function(email) {
+        return "http://www.gravatar.com/avatar/" + CryptoJS.MD5(email);
+    },
+    
+    
+    /**
+     * convert date in vor n Minuten
+     */
+    dateAgo: function(date) {
+        var now = new Date().getTime() / 1000;
+        
+        var ageInSeconds = now - date;
+        var ageInMinutes = ageInSeconds / 60;
+        var ageInHours = ageInMinutes / 60;
+        var ageInDays = ageInHours / 24;
+        
+        if(ageInMinutes<1)
+            return 'vor ' + Math.floor(ageInSeconds) + ' Sekunden';
+        if(ageInHours<1)
+            return 'vor ' + Math.floor(ageInMinutes) + ' Minuten';
+        if(ageInDays<1)
+            return 'vor ' + Math.floor(ageInHours) + ' Stunden';
+        
+        var dateObj = new Date(date*1000);
+        return dateObj.getHours() + ':' + dateObj.getMinutes() + ':' + dateObj.getSeconds();
+    },
+    
+    
+    /**
+     * escape html
+     */
+    escape: function(string) {
+        var entityMap = {
+            "&": "&amp;",
+            "<": "&lt;",
+            ">": "&gt;",
+            '"': '&quot;',
+            "'": '&#39;',
+            "/": '&#x2F;'
+          };
+
+        return String(string).replace(/[&<>"'\/]/g, function (s) {
+          return entityMap[s];
+        });
+    },
+    
+    
+    /**
+     * emoticons einfuegen
+     */
+    emoticons: function(text) {
+        $.each(emoticons, function(shortcut, emoticon) {
+            text = text.replace(shortcut, '<img src="'+ emoticon +'" title="' + shortcut + '"/>');
+        });
+        return text;
     }
 }
